9aa86e565fd52a2902313436001b475adff30998
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-07-11 23:41:07 +0900

    feat: completed hero.riot with hero service

--- src/services/hero.service.js
+++ src/services/hero.service.js
@@ -1,5 +1,20 @@
 import { HEROES } from "@/components/global/heroes/mock-heroes";
+import observable from "@riotjs/observable";
 
-export const getHeroes = () => {
-  return HEROES;
+const heroService = {
+  async getHeroes() {
+    try {
+      // 実際は以下のように実装するが，今回はモックで実装
+      // const response = await fetch('https://api.example.com/heroes');
+      // const heroes = await response.json();
+      const heroes = HEROES;
+      this.trigger('heroesUpdated', heroes)
+    } catch (error) {
+      console.error('Failed to fetch heroes:', error);
+    }
+  }
 };
+
+observable(heroService);
+
+export default heroService;
